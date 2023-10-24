05eda1f2c9d8a4b298ecfc383c7ef65763a3bf12
DPaul-11 <125825050+DPaul-11@users.noreply.github.com>
2023-10-24 22:42:36 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,30 @@
+class Account {
+    private long acc_no;
+    private String name;
+    private long amount;
+    public long getAcc_no()
+    {
+        return acc_no;
+    }
+    public void setAcc_no(long acc_no)
+    {
+        this.acc_no=acc_no;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public long getAmount()
+    {
+        return amount;
+    }
+    public void setAmount(long amount)
+    {
+        this.amount=amount;
+    }
+
+}
